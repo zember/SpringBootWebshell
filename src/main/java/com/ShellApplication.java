5926245c6e5c7a76e579913c31e8be4bc4f094b8
zember <zemberster@gmail.com>
2023-01-02 22:10:54 +0100

    Update ShellApplication.java

--- src/main/java/com/ShellApplication.java
+++ src/main/java/com/ShellApplication.java
@@ -12,6 +12,10 @@ public class ShellApplication {
         settings.getOrDefault("APP_PORT", "8012");
         settings.getOrDefault("APP_NAME", "SpringBootWebshell");
         settings.getOrDefault("APP_ENV", "local");
-        SpringApplication.run(ShellApplication.class,args);
+        SpringApplication app = new SpringApplication(ShellApplication.class);
+        app.setDefaultProperties(Collections
+          .singletonMap("server.port", settings.getOrDefault("APP_PORT", "8012")));
+        app.run(args);
+        //SpringApplication.run(ShellApplication.class,args);
     }
 }
